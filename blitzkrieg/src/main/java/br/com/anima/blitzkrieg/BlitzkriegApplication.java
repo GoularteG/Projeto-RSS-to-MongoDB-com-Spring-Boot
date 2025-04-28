58ad3e66032e65192edd0d0ebc656c1382824468
Gustavo <goulartegustavo20@gmail.com>
2025-04-28 17:49:37 -0300

    "Ajustando a BlitzkriegApplication.java"

--- blitzkrieg/src/main/java/br/com/anima/blitzkrieg/BlitzkriegApplication.java
+++ blitzkrieg/src/main/java/br/com/anima/blitzkrieg/BlitzkriegApplication.java
@@ -1,13 +1,24 @@
 package br.com.anima.blitzkrieg;
 
+import br.com.anima.blitzkrieg.service.RssService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlitzkriegApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlitzkriegApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run(RssService rssService) {
+		return args -> {
+			String rssUrl = "https://www.logisticsmgmt.com/rss"; // Exemplo
+			rssService.fetchAndSaveRss(rssUrl);
+			System.out.println("✅ RSS processado e salvo no MongoDB!");
+		};
+	}
 }
+
