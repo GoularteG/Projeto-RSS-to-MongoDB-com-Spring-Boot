364450837704a1eb83667731fc649813be472fb4
Gustavo <goulartegustavo20@gmail.com>
2025-04-28 17:50:02 -0300

    "Tratando erros de URL"

--- blitzkrieg/src/main/java/br/com/anima/blitzkrieg/service/RssService.java
+++ blitzkrieg/src/main/java/br/com/anima/blitzkrieg/service/RssService.java
@@ -4,10 +4,14 @@ import br.com.anima.blitzkrieg.model.Rss;
 import br.com.anima.blitzkrieg.repository.RssRepository;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 @Service
@@ -17,11 +21,13 @@ public class RssService {
     private RssRepository rssRepository;
 
     public void fetchAndSaveRss(String rssUrl) {
+        if (rssUrl == null || rssUrl.isBlank()) {
+            throw new IllegalArgumentException("URL do RSS não pode ser vazia!");
+        }
         try {
             URL feedUrl = new URL(rssUrl);
             SyndFeedInput input = new SyndFeedInput();
             SyndFeed feed = input.build(new XmlReader(feedUrl));
-
             for (SyndEntry entry : feed.getEntries()) {
                 Rss rssFeed = new Rss();
                 rssFeed.setTitulo(entry.getTitle());
@@ -31,8 +37,15 @@ public class RssService {
 
                 rssRepository.save(rssFeed);
             }
+        }  catch (MalformedURLException e) {
+            System.err.println("URL mal formatada: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Erro de conexão com o feed RSS: " + e.getMessage());
+        } catch (FeedException e) {
+            System.err.println("Erro ao processar o XML do RSS: " + e.getMessage());
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println("Erro inesperado: " + e.getMessage());
         }
+
     }
 }
